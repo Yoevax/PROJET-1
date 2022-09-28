40aeaf03a41d589120f66aa7b063e02fcd6c8a68
Yousra Lemqaddem <lemqaddem.yousra@gmail.com>
2022-09-28 11:11:20 +0200

    rajout new pages

---
+++ SRC/database.php
@@ -0,0 +1,17 @@
+<?php
+try {
+
+    $mysqldb = new PDO(
+        sprintf(
+            'mysql:host=%s;dbname=%s;port=%s',
+            "localhost",
+            "findme",
+            3306
+        ),
+        "root",
+        "root"
+    );
+    $mysqldb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $exception) {
+    die('Oops, il y a eu problème : ' . $exception->getMessage());
+}
